b74b079799f3f453177078f009b2e189c520b6eb
sman01 <shreemun@gmail.com>
2020-04-28 18:07:10 +0530

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('nefjew'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("cash money"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/models_gw/images-s/rid/aventador_2.jpg',
+              ),
+            ),
+          ),
         ),
       ),
     );
